fa2d6d28262a43b3a15d1e4fcac7c9a6c4ac2563
Patipark Sopha <iamwad@gmail.com>
2025-04-06 20:54:38 +0700

    ปรับช่องว่างระหว่างบรรดทัดด้วย leading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <div className='bg-[#F5F7FA] h-auto md:h-[37.5rm]'>
         <div className='container mx-auto max-w-[2320px] p-10'>
-            <div><h1 className='text-[4rem] font-semibold text-[#4D4D4D]'>Lessons and insights <br/><span className='text-[#4CAF4F]'>from 8 years</span></h1></div>
+            <div><h1 className='text-[4rem] font-semibold text-[#4D4D4D] leading-[1]'>Lessons and insights <br/><span className='text-[#4CAF4F]'>from 8 years</span></h1></div>
             <div></div>
         </div>
 
