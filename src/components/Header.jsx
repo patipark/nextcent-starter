67983abb54a83f4f9c0cc45017dabef06fdaa2d4
Patipark Sopha <iamwad@gmail.com>
2025-04-06 20:49:55 +0700

    เพิ่มกล่อง container เข้าไปแบบ สูง auto ในจอเล็ก และ สูง 37.5rem ในจอใหญ่ และใส่ container เข้าไปด้านในโดยให้มีความกล้างสูงสุด 2320px ระยะขอบ 10px และเพิ่ม container เข้าด้านในอีก ​2 div โดยตัวแรกให้มี h1 4rem semibold สี #4D4D4D

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 function Header() {
   return (
     <div className='bg-[#F5F7FA] h-auto md:h-[37.5rm]'>
+        <div className='container mx-auto max-w-[2320px] p-10'>
+            <div><h1 className='text-[4rem] font-semibold text-[#4D4D4D]'>Lessons and insights <br/>from 8 years</h1></div>
+            <div></div>
+        </div>
 
     </div>
   )
