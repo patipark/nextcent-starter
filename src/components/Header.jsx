5e3b6819f2b40889425abc564e94a10eb3da3f3e
Patipark Sopha <iamwad@gmail.com>
2025-04-06 21:16:50 +0700

    หน้าจอเล็กให้เรียง items ตรงกลาง ส่วนหน้าจอใหญ่ไม่ให้มี margin (md:p-0)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import HeaderImg from '../assets/img/header-img.png'
 
 function Header() {
   return (
-    <div className='bg-[#F5F7FA] h-auto md:h-[37.5rm]'>
-        <div className='container mx-auto max-w-[2320px] p-10 flex flex-col md:flex-row md:justify-between items-center'>
+    <div className='bg-[#F5F7FA] h-auto md:h-[37.5rm] flex items-center'>
+        <div className='container mx-auto max-w-[2320px] p-10 flex flex-col md:flex-row md:justify-between items-center md:p-0'>
             <div>
                 <h1 className='text-[4rem] font-semibold text-[#4D4D4D] leading-[1]'>Lessons and insights <br/><span className='text-[#4CAF4F]'>from 8 years</span></h1>
                 <p className='text-[#717171] mt-5'>Where to grow your business as a photographer: site or social media?</p>
