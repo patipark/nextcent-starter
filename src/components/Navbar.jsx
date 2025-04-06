fd02395a4a272a010d6378fbf105d30fa7ad3593
Patipark Sopha <iamwad@gmail.com>
2025-04-06 12:20:11 +0700

    วาง logo กับ เมนู แต่ยังเรียงในแนวตั้งอยู่

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Logo from "../assets/img/logo-black.png";
+import { FaBars } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <nav className="bg-[#F5F7FA]">
+      <div className="container mx-auto max-w-[1320px] relative h-auto p-10">
+        <div>
+            <a href="#">
+                <img src={Logo} alt=""/>
+            </a>
+        </div>
+        <ui>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Service</a></li>
+            <li><a href="#">Feature</a></li>
+            <li><a href="#">Product</a></li>
+            <li><a href="#">Testimonial</a></li>
+            <li><a href="#">Testimonial</a></li>
+            <li><a href="#">FAQ</a></li>
+        </ui>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
