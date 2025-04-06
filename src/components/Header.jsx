de2a1670ae263cc359a907f62236b555cc570985
Patipark Sopha <iamwad@gmail.com>
2025-04-06 21:13:19 +0700

    ถ้าจอเล็กให้เรียง flex แนวตั้ง ถ้าจอใหญ่ให้เรียง flex แนวนอน และจอใหญ่ให้กระจายออกระหว่างซ้ายขวา justify-between

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import HeaderImg from '../assets/img/header-img.png'
 function Header() {
   return (
     <div className='bg-[#F5F7FA] h-auto md:h-[37.5rm]'>
-        <div className='container mx-auto max-w-[2320px] p-10'>
+        <div className='container mx-auto max-w-[2320px] p-10 flex flex-col md:flex-row md:justify-between'>
             <div>
                 <h1 className='text-[4rem] font-semibold text-[#4D4D4D] leading-[1]'>Lessons and insights <br/><span className='text-[#4CAF4F]'>from 8 years</span></h1>
                 <p className='text-[#717171] mt-5'>Where to grow your business as a photographer: site or social media?</p>
