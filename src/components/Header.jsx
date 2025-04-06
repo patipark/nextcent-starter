39e50c1bad035a1d55fdf67fdf64acff0add398b
Patipark Sopha <iamwad@gmail.com>
2025-04-06 20:31:55 +0700

    convert pixcel to rem 600px=37.5rem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='bg-[#F5F7FA] h-auto md:h-[600px]'>
+    <div className='bg-[#F5F7FA] h-auto md:h-[37.5rm]'>
 
     </div>
   )
