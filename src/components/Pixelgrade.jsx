662985edbdaf24521bb332e3e7b3c6270b0d5b16
Patipark Sopha <iamwad@gmail.com>
2025-04-07 11:28:16 +0700

    import image และปรับ reponsive เหมือน component อื่นๆ

--- src/components/Pixelgrade.jsx
+++ src/components/Pixelgrade.jsx
@@ -1,8 +1,15 @@
+import img01 from '../assets/img/img-01.png'
 
 const Pixelgrade = () => {
   return (
     <div className='py-10'>
-        <div className="container mx-auto max-w-[1320px] p-10">
+        <div className="container mx-auto max-w-[1320px] p-10 grid grid-cols-1 md:grid-cols-2 md:justify-between items-center md:p-0">
+            <div><img src={img01} alt='' /></div>
+            <div>
+                <h3 className='mt-5 md:mt-0 font-semibold text-[2.25rem] text-[#4D4D4D]'>The unseen of spending three <br></br>years at Pixelgrade</h3>
+                <p className='text-[#717171] my-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+                <a className="inline-flex justify-center items-center py-3 px-8 mt-5 bg-[#4CAF4F] text-white rounded-md" href="#">Learn More</a>
+            </div>
 
         </div>
     </div>
