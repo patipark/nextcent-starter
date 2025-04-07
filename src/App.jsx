67d419dc20767084caa5416bf16db857b4e16dd2
Patipark Sopha <iamwad@gmail.com>
2025-04-07 11:03:15 +0700

    create Pixelgrade component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import Header from './components/Header'
 import Clients from './components/Clients'
 import Comunity from './components/Comunity'
+import Pixelgrade from './components/Pixelgrade'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Header />
       <Clients />
       <Comunity />
+      <Pixelgrade />
     </>
   )
 }
---
+++ src/components/Pixelgrade.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pixelgrade = () => {
+  return (
+    <div>Pixelgrade</div>
+  )
+}
+
+export default Pixelgrade
