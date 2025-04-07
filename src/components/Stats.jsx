9eafefa13f5964b734ffb2fca89c925532174e17
Patipark Sopha <iamwad@gmail.com>
2025-04-07 11:37:51 +0700

    สร้าง div ตัวครอบด้านนอกสุดก่อน โดยให้ระยะห่างขอบบนล่าง 10px, ระยะห่างขอบซ้ายขวา 5px และถ้าเป็นจอใหญ่จะไม่มีระยะห่างขอบซ้ายขวา

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import icon04 from '../assets/img/icon-04.png'
+import icon05 from '../assets/img/icon-05.png'
+import icon06 from '../assets/img/icon-06.png'
+import icon07 from '../assets/img/icon-07.png'
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <div className='bg-[#F5F7FA] py-10 px-5 md:px-0'>
+
+    </div>
   )
 }
 
