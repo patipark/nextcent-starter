aba68c67a3f2fd941d430930788d84be7e9628af
Patipark Sopha <iamwad@gmail.com>
2025-04-07 11:42:05 +0700

    สร้าง container มาครอบ div 2 ตัว ด้านในไว้ โดย container นี้จะเป็นตัวกำหนด ความกว้าง auto และสูงสุด 1320px

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -6,9 +6,10 @@ import icon07 from '../assets/img/icon-07.png'
 const Stats = () => {
   return (
     <div className='bg-[#F5F7FA] py-10 px-5 md:px-0'>
+     <div className='container mx-auto max-w-[1320px]'>
       <div></div>
       <div></div>
-
+     </div>
     </div>
   )
 }
